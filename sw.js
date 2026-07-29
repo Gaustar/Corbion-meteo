8f4ab1808c605fa206a93b319a48eda9e2e02622
Gaustar <52787279+Gaustar@users.noreply.github.com>
2026-07-29 06:16:24 +0200

    Fix formatting issues in service worker file

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
 const CACHE_NAME = 'skystation-v1';
 const urlsToCache = ['./', './index.html', './manifest.webmanifest'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
